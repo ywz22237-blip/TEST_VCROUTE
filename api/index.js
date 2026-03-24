30a236a22ee28e8fb8491b1cd01260ac1b491538
venture <venture@Ventureplatform.co.kr>
2026-03-24 17:46:14 +0900

    debug: expose startup error in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
-const app = require('../backend/src/app');
+let app;
+try {
+  app = require('../backend/src/app');
+} catch (e) {
+  app = (req, res) => {
+    res.status(500).json({ error: e.message, stack: e.stack });
+  };
+}
 
 module.exports = app;
